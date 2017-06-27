bbcd02afa30d3783abbdc55a62b8f926283e6a7d
Bill McDowell <forkunited@gmail.com>
2017-06-27 15:05:39 -0700

    Added annotation script

---
+++ src/test/py/ltprg/game/color/data/annotate.py
@@ -0,0 +1,14 @@
+import unittest
+import sys
+import time
+import mung.nlp.corenlp
+
+input_data_dir = sys.argv[1]
+output_data_dir = sys.argv[2]
+
+annotator = mung.nlp.corenlp.CoreNLPAnnotator('records[*].events[?type = "Utterance"]', 'contents', 'nlp')
+annotator.annotate_directory(input_data_dir, output_data_dir)
+
+if __name__ == '__main__':
+    unittest.main()
+
