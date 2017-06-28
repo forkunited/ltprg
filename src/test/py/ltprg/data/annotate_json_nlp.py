4b89de16578eeddc72413188221215ca65a8e239
Bill McDowell <wmcdowel@sherlock-ln03.stanford.edu>
2017-06-28 11:49:09 -0700

    Added some annotated data

--- src/test/py/ltprg/data/annotate_json_nlp.py
+++ src/test/py/ltprg/data/annotate_json_nlp.py
@@ -1,4 +1,3 @@
-import unittest
 import sys
 import time
 import mung.nlp.corenlp
@@ -6,9 +5,7 @@ import mung.nlp.corenlp
 input_data_dir = sys.argv[1]
 output_data_dir = sys.argv[2]
 
-annotator = mung.nlp.corenlp.CoreNLPAnnotator('records[*].events[?type = "Utterance"]', 'contents', 'nlp')
-annotator.annotate_directory(input_data_dir, output_data_dir)
+annotator = mung.nlp.corenlp.CoreNLPAnnotator('$.records[*].events[?type = "Utterance"]', 'contents', 'nlp')
+annotator.annotate_directory(input_data_dir, output_data_dir, id_key="gameid")
 
-if __name__ == '__main__':
-    unittest.main()
 
