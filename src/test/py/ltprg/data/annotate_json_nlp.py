8994647ba4833ffe93bb8b0cdadefc2c30bfa513
Bill McDowell <forkunited@gmail.com>
2017-07-09 12:21:43 -0700

    Cleaned up game featurization code

--- src/test/py/ltprg/data/annotate_json_nlp.py
+++ src/test/py/ltprg/data/annotate_json_nlp.py
@@ -7,5 +7,3 @@ output_data_dir = sys.argv[2]
 
 annotator = mung.nlp.corenlp.CoreNLPAnnotator('$.records[*].events[?type = "Utterance"]', 'contents', 'nlp')
 annotator.annotate_directory(input_data_dir, output_data_dir, id_key="gameid")
-
-
---
+++ src/test/py/ltprg/game/color/data/featurize_sua.py
@@ -0,0 +1,92 @@
+import unittest
+import numpy as np
+import sys
+import mung.feature_helpers
+from os.path import join
+
+input_data_dir = sys.argv[1]
+output_feature_dir = sys.argv[2]
+partition_file = sys.argv[3]
+
+# Necessary to allow unittest.main() to work
+del sys.argv[3]
+del sys.argv[2]
+del sys.argv[1]
+
+np.random.seed(1)
+
+class FeaturizeSUA(unittest.TestCase):
+    def test_utterance_lemmas:
+        mung.feature_helpers.featurize_path_enum_seqs(
+            input_data_dir,
+            join(output_feature_dir, "utterance_lemmas"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "utterance_lemmas"
+            ["utterances[*].nlp.lemmas.lemmas"],
+            15, # Maximum utterance length
+            token_fn=lambda x : x.lower())
+
+    def test_listener_clicked:
+        mung.feature_helpers.featurize_path_scalars(
+            input_data_dir,
+            join(output_feature_dir, "listener_clicked"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "listener_clicked",
+            ["action.lClicked_0", "action.lClicked_1", "action.lClicked_2"])
+
+    def test_listener_colors:
+        mung.feature_helpers.featurize_path_scalars(
+            input_data_dir,
+            join(output_feature_dir, "listener_colors"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "listener_colors",
+            ["state.lH_0","state.lS_0","state.lL_0",
+             "state.lH_1","state.lS_1","state.lL_1",
+             "state.lH_2","state.lS_2","state.lL_2"])
+
+    def test_speaker_colors:
+        mung.feature_helpers.featurize_path_scalars(
+            input_data_dir,
+            join(output_feature_dir, "speaker_colors"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "speaker_colors",
+            ["state.sH_0","state.sS_0","state.sL_0",
+             "state.sH_1","state.sS_1","state.sL_1",
+             "state.sH_2","state.sS_2","state.sL_2"])
+
+    def test_speaker_observed:
+        mung.feature_helpers.featurize_path_scalars(
+            input_data_dir,
+            join(output_feature_dir, "speaker_observed"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "speaker_observed",
+            ["state.sH_0","state.sS_0","state.sL_0",
+             "state.sH_1","state.sS_1","state.sL_1",
+             "state.sH_2","state.sS_2","state.sL_2",
+             "state.sTarget_0","state.sTarget_1","state.sTarget_2"])
+
+    def speaker_target_color:
+        mung.feature_helpers.featurize_path_scalars(
+            input_data_dir,
+            join(output_feature_dir, "speaker_target_color"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "speaker_target_color",
+            ["state.sTarget_H","state.sTarget_S","state.sTarget_L"])
+
+    def speaker_target:
+        mung.feature_helpers.featurize_path_scalars(
+            input_data_dir,
+            join(output_feature_dir, "speaker_carget"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "speaker_target",
+            ["state.sTarget_0","state.sTarget_1","state.sTarget_2"])
+
+if __name__ == '__main__':
+    unittest.main()
