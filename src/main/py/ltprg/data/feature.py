290c33ef02ed3cc9ad676ae805092a7f4e25a11e
katherine-h <hermannk@stanford.edu>
2017-09-05 19:13:49 -0700

    Minor edits

--- src/main/py/ltprg/data/feature.py
+++ src/main/py/ltprg/data/feature.py
@@ -1,6 +1,8 @@
 from mung.feature import FeatureType
 import sys
 sys.path.append('../../../../test/py/ltprg/game/color/properties/')
+import numpy as np
+import time
 from alexnet import PartialAlexnet, rgb_to_alexnet_input
 from colorspace_conversions import hsls_to_rgbs
 
@@ -18,7 +20,11 @@ class VisualEmbedding(FeatureType):
 		output = []
 		for color in self._paths:
 			hsl = [datum.get(dim) for dim in color]
-			output.extend(self.cnn.forward(rgb_to_alexnet_input(hsl)
+			if hsl == [None, None, None]:
+				print hsl
+				print datum.get("id")
+			rgb = np.array(hsls_to_rgbs([map(int, hsl)]))[0]
+			output.extend(self.cnn.forward(rgb_to_alexnet_input(rgb)
 							).data.numpy()[0])
 		vec[start_index : len(vec)] = output[start_index : len(output)]
 
@@ -39,7 +45,7 @@ class VisualEmbedding(FeatureType):
 	def init_start(self):
 		self.cnn = PartialAlexnet('fc6')
 
-	def init_datum(self):
+	def init_datum(self, datum):
 		pass
 
 	def init_end(self):
