56d445704192a27a79b0da41d7515cffc1bbfd34
Bill McDowell <forkunited@gmail.com>
2017-12-29 11:29:03 -0800

    Added shape grids featurization

--- src/test/py/ltprg/data/make_sua.py
+++ src/test/py/ltprg/data/make_sua.py
@@ -33,8 +33,33 @@ utterance_mode = sys.argv[1]
 input_data_dir = sys.argv[2]
 output_data_dir = sys.argv[3]
 exclude_missing_utts = bool(sys.argv[4])
+exclude_missing_actions = bool(sys.argv[5])
 
 np.random.seed(1) # Ensures data loaded in same order each time
+
+def make_sua_properties(game_id, roundNum, sua_index, cur_state, cur_utts, cur_action):
+    sua_properties = dict()
+    sua_properties["id"] = game_id + "_" + str(roundNum) + "_" + str(sua_index)
+    sua_properties["gameid"] = game_id
+    sua_properties["roundNum"] = roundNum
+    sua_properties["sua"] = sua_index
+    sua_properties["state"] = cur_state
+
+    if utterance_mode == UTTERANCES_ALL:
+        sua_properties["utterances"] = cur_utts
+    elif utterance_mode == UTTERANCES_SPEAKER:
+        sua_properties["utterances"] = [cur_utt for cur_utt in cur_utts if cur_utt["sender"] == "speaker"]
+    elif utterance_mode == UTTERANCES_LISTENER:
+        sua_properties["utterances"] = [cur_utt for cur_utt in cur_utts if cur_utt["sender"] == "listener"]
+    elif utterance_mode == UTTERANCES_SPEAKER_LAST and has_speaker:
+        sua_properties["utterances"] = [next(cur_utt for cur_utt in reversed(cur_utts) if cur_utt["sender"] == "speaker")]
+    elif utterance_mode == UTTERANCES_LISTENER_LAST and has_listener:
+        sua_properties["utterances"] = [next(cur_utt for cur_utt in reversed(cur_utts) if cur_utt["sender"] == "listener")]
+    else:
+        sua_properties["utterances"] = []
+
+    sua_properties["action"] = cur_action
+    return sua_properties
 
 game_data = DataSet.load(input_data_dir, id_key="gameid")
 sua_datums = []
@@ -48,9 +73,11 @@ for i in range(game_data.get_size()):
         sua_index = 0
         cur_utts = []
         cur_state = None
+        cur_action = None
         has_speaker = False
         has_listener = False
         prev_utt_count = 0
+        no_action = True
         for event in record["events"]:
             if event["type"].startswith("State"):
                 cur_state = event
@@ -61,29 +88,9 @@ for i in range(game_data.get_size()):
                 elif event["sender"] == "listener":
                     has_listener = True
             else:
+                no_action = False
                 cur_action = event
-
-                sua_properties = dict()
-                sua_properties["id"] = game_id + "_" + str(roundNum) + "_" + str(sua_index)
-                sua_properties["gameid"] = game_id
-                sua_properties["roundNum"] = roundNum
-                sua_properties["sua"] = sua_index
-                sua_properties["state"] = cur_state
-
-                if utterance_mode == UTTERANCES_ALL:
-                    sua_properties["utterances"] = cur_utts
-                elif utterance_mode == UTTERANCES_SPEAKER:
-                    sua_properties["utterances"] = [cur_utt for cur_utt in cur_utts if cur_utt["sender"] == "speaker"]
-                elif utterance_mode == UTTERANCES_LISTENER:
-                    sua_properties["utterances"] = [cur_utt for cur_utt in cur_utts if cur_utt["sender"] == "listener"]
-                elif utterance_mode == UTTERANCES_SPEAKER_LAST and has_speaker:
-                    sua_properties["utterances"] = [next(cur_utt for cur_utt in reversed(cur_utts) if cur_utt["sender"] == "speaker")]
-                elif utterance_mode == UTTERANCES_LISTENER_LAST and has_listener:
-                    sua_properties["utterances"] = [next(cur_utt for cur_utt in reversed(cur_utts) if cur_utt["sender"] == "listener")]
-                else:
-                    sua_properties["utterances"] = []
-
-                sua_properties["action"] = cur_action
+                sua_properties = make_sua_properties(game_id, roundNum, sua_index, cur_state, cur_utts, cur_action)
 
                 prev_utt_count = len(sua_properties["utterances"])
 
@@ -92,13 +99,20 @@ for i in range(game_data.get_size()):
 
                 sua_index += 1
                 cur_state = None
+                cur_action = None
                 cur_utts = []
                 has_speaker = False
                 has_listener = False
 
-        if len(cur_utts) > 0 and prev_utt_count == 0:
-            hanging_speaker_utts += len([cur_utt for cur_utt in cur_utts if cur_utt["sender"] == "speaker"])
-            hanging_listener_utts += len([cur_utt for cur_utt in cur_utts if cur_utt["sender"] == "listener"])
+        if len(cur_utts) > 0:
+            if no_action:
+                if not exclude_missing_actions:
+                    sua_properties = make_sua_properties(game_id, roundNum, sua_index, cur_state, cur_utts, cur_action)
+                    if (not exclude_missing_utts) or len(sua_properties["utterances"]) > 0:
+                        sua_datums.append(Datum(properties=sua_properties))
+            elif prev_utt_count == 0:
+                hanging_speaker_utts += len([cur_utt for cur_utt in cur_utts if cur_utt["sender"] == "speaker"])
+                hanging_listener_utts += len([cur_utt for cur_utt in cur_utts if cur_utt["sender"] == "listener"])
 
 print "NOTE: " + str(hanging_listener_utts) + " hanging listener utterances and " + str(hanging_speaker_utts) + " hanging speaker utterances"
 
---
+++ src/test/py/ltprg/game/shapeGrids/data/featurize_sua.py
@@ -0,0 +1,111 @@
+import unittest
+import numpy as np
+import sys
+import mung.feature_helpers
+from os.path import join
+
+input_data_dir = sys.argv[1]
+output_feature_dir = sys.argv[2]
+partition_file = sys.argv[3]
+
+# Necessary to allow unittest.main() to work
+del sys.argv[3]
+del sys.argv[2]
+del sys.argv[1]
+
+np.random.seed(1)
+
+class FeaturizeSUA(unittest.TestCase):
+
+    # Constructs sequences of integer indices representing cleaned utterances
+    def test_utterance_clean_indices(self):
+        print "Featurizing utterance clean indices"
+        mung.feature_helpers.featurize_path_enum_seqs(
+            input_data_dir, # Source data set
+            join(output_feature_dir, "utterance_clean_idx"), # Output directory
+            partition_file, # Data partition
+            lambda d : d.get("gameid"), # Function that partitions the data
+            "utterance_clean_idx", # Name of the feature
+            ["utterances[*].nlp.clean_strs.strs"], # JSON path into data examples
+            30, # Maximum utterance length
+            token_fn=lambda x : x, # Function applied to tokens to construct the vocabulary
+            indices=True) # Indicates that indices will be computed instead of one-hot vectors
+
+    # Constructs sequences of integer indices representing last cleaned utterances
+    #def test_utterance_clean_last_indices(self):
+    #    print "Featurizing utterance clean last indices"
+    #    mung.feature_helpers.featurize_path_enum_seqs(
+    #        input_last_data_dir, # Source data set
+    #        join(output_feature_dir, "utterance_clean_last_idx"), # Output directory
+    #        partition_file, # Data partition
+    #        lambda d : d.get("gameid"), # Function that partitions the data
+    #        "utterance_clean_last_idx", # Name of the feature
+    #        ["utterances[*].nlp.clean_strs.strs"], # JSON path into data examples
+    #        24, # Maximum utterance length
+    #        token_fn=lambda x : x, # Function applied to tokens to construct the vocabulary
+    #        indices=True) # Indicates that indices will be computed instead of one-hot vectors
+
+    # Constructs indicators of which color (0, 1, or 2) that the listener clicked
+    #def test_listener_clicked(self):
+    #    print "Featurizing listener clicks"
+    #    mung.feature_helpers.featurize_path_scalars(
+    #        input_data_dir, # Source data set
+    #        join(output_feature_dir, "listener_clicked"), # Output directory
+    #        partition_file, # Data partition
+    #        lambda d : d.get("gameid"), # Function that partitions the data
+    #        "listener_clicked", # Name of the feature
+    #        ["action.lClicked_0", "action.lClicked_1", "action.lClicked_2"]) # JSON paths to feature values
+
+    # Constructs indices of which color (0, 1, or 2) that the listener clicked
+    #def test_listener_clicked_indices(self):
+    #    print "Featurizing listener click indices"
+    #    mung.feature_helpers.featurize_path_scalars(
+    #        input_data_dir, # Source data set
+    #        join(output_feature_dir, "listener_clicked_idx"), # Output directory
+    #        partition_file, # Data partition
+    #        lambda d : d.get("gameid"), # Function that partitions the data
+    #        "listener_clicked_idx", # Name of the feature
+    #        ["action.lClickedIndex"]) # JSON paths to feature values
+
+    # Constructs vectors of the colors that the speaker observed
+    def test_speaker_colors(self):
+        print "Featurizing speaker colors"
+        mung.feature_helpers.featurize_path_scalars(
+            input_data_dir,
+            join(output_feature_dir, "speaker_colors"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "speaker_colors",
+            ["state.sObj0_Shp0_ClrH","state.sObj0_Shp0_ClrS",
+             "state.sObj0_Shp1_ClrH","state.sObj0_Shp1_ClrS",
+             "state.sObj0_Shp2_ClrH","state.sObj0_Shp2_ClrS",
+             "state.sObj0_Shp3_ClrH","state.sObj0_Shp3_ClrS",
+             "state.sObj0_Shp4_ClrH","state.sObj0_Shp4_ClrS",
+             "state.sObj0_Shp5_ClrH","state.sObj0_Shp5_ClrS",
+             "state.sObj0_Shp6_ClrH","state.sObj0_Shp6_ClrS",
+             "state.sObj0_Shp7_ClrH","state.sObj0_Shp7_ClrS",
+             "state.sObj0_Shp8_ClrH","state.sObj0_Shp8_ClrS",
+             "state.sObj1_Shp0_ClrH","state.sObj1_Shp0_ClrS",
+             "state.sObj1_Shp1_ClrH","state.sObj1_Shp1_ClrS",
+             "state.sObj1_Shp2_ClrH","state.sObj1_Shp2_ClrS",
+             "state.sObj1_Shp3_ClrH","state.sObj1_Shp3_ClrS",
+             "state.sObj1_Shp4_ClrH","state.sObj1_Shp4_ClrS",
+             "state.sObj1_Shp5_ClrH","state.sObj1_Shp5_ClrS",
+             "state.sObj1_Shp6_ClrH","state.sObj1_Shp6_ClrS",
+             "state.sObj1_Shp7_ClrH","state.sObj1_Shp7_ClrS",
+             "state.sObj1_Shp8_ClrH","state.sObj1_Shp8_ClrS"])
+
+    # Constructs an index of the target color index (0, 1, or 2) that
+    # the speaker observed
+    def test_speaker_target_indices(self):
+        print "Featurizing speaker target indices"
+        mung.feature_helpers.featurize_path_scalars(
+            input_data_dir,
+            join(output_feature_dir, "speaker_target_idx"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "speaker_target_idx",
+            ["state.sTarget"])
+
+if __name__ == '__main__':
+    unittest.main()
