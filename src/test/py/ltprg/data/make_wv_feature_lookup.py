9e79aaf895a8c6b3c891ca49f54b9e441023e6d3
Bill McDowell <forkunited@gmail.com>
2018-02-24 13:02:00 -0800

    Added snli featurization and glove preprocessing

---
+++ src/test/py/ltprg/data/make_wv_feature_lookup.py
@@ -0,0 +1,34 @@
+import numpy as np
+from mung.feature import FeatureSequenceSet
+
+feature_seq_dir= sys.argv[1]
+wv_file = sys.argv[2]
+output_file = sys.argv[3]
+
+def load_wv():
+    token_to_wv = dict()
+    wv_size = 0
+    with open(wv_file, 'r') as fp:
+        for line in fp:
+            line_parts = line.split(' ')
+            token = line_parts[0]
+            wv = np.array([float(line_parts[i]) for i in range(1, len(line_parts))])
+            token_to_wv[token] = wv
+            wv_size = wv.shape[0]
+    return token_to_wv, wv_size
+
+token_to_wv, wv_size = load_wv()
+feature_seq = FeatureSequenceSet.load(feature_seq_dir)
+vocab_size = feature_seq.get_feature_set_size()
+wv_mat = np.array(shape=(vocab_size, wv_size))
+
+for i in range(vocab_size):
+    token = feature_seq.get_feature_token(i).get_value()
+    wv = None
+    if token in token_to_wv:
+        wv = token_to_wv[token]
+    else:
+        wv = np.random.normal(loc=0.0, scale=1.0, size=(wv_size))
+    wv_mat[i] = wv
+
+np.save(output_file, wv_mat)
---
+++ src/test/py/ltprg/game/snli/data/featurize_sua.py
@@ -0,0 +1,63 @@
+import unittest
+import numpy as np
+import sys
+import mung.feature_helpers
+import ltprg.data.feature_helpers
+from os.path import join
+
+input_data_dir = sys.argv[1]
+output_feature_dir = sys.argv[2]
+partition_file = sys.argv[3]
+
+# Necessary to allow unittest.main() to work
+del sys.argv[4]
+del sys.argv[3]
+del sys.argv[2]
+del sys.argv[1]
+
+np.random.seed(1)
+
+class FeaturizeSUA(unittest.TestCase):
+
+    # Constructs sequences of integer indices representing utterances
+    def test_utterance_indices(self):
+        print "Featurizing utterance indices"
+        mung.feature_helpers.featurize_path_enum_seqs(
+            input_data_dir, # Source data set
+            join(output_feature_dir, "utterance_idx"), # Output directory
+            partition_file, # Data partition
+            lambda d : d.get("gameid"), # Function that partitions the data
+            "utterance_idx", # Name of the feature
+            ["utterance.nlp.token_strs.strs"], # JSON path into data examples
+            40, # Maximum utterance length
+            token_fn=lambda x : x, # Function applied to tokens to construct the vocabulary
+            indices=True) # Indicates that indices will be computed instead of one-hot vectors
+
+    # Constructs sequences of integer indices representing utterances
+    def test_premise_indices(self):
+        print "Featurizing premise indices"
+        mung.feature_helpers.featurize_path_enum_seqs(
+            input_data_dir, # Source data set
+            join(output_feature_dir, "premise_idx"), # Output directory
+            partition_file, # Data partition
+            lambda d : d.get("gameid"), # Function that partitions the data
+            "premise_idx", # Name of the feature
+            ["state.nlp.token_strs.strs"], # JSON path into data examples
+            40, # Maximum utterance length
+            token_fn=lambda x : x, # Function applied to tokens to construct the vocabulary
+            indices=True) # Indicates that indices will be computed instead of one-hot vectors
+
+    # Constructs an index of the target color index (0, 1, or 2) that
+    # the speaker observed
+    def test_speaker_target_indices(self):
+        print "Featurizing speaker target indices"
+        mung.feature_helpers.featurize_path_scalars(
+            input_data_dir,
+            join(output_feature_dir, "speaker_target_idx"),
+            partition_file,
+            lambda d : d.get("gameid"),
+            "speaker_target_idx",
+            ["state.sTarget"])
+
+if __name__ == '__main__':
+    unittest.main()
