77bae6ccc2f8bb4b2f8fb30e72601ac013041835
Bill McDowell <wmcdowel@sherlock-ln04.stanford.edu>
2018-04-22 14:44:49 -0700

    Small change to color conversion

--- src/test/py/ltprg/game/colorGrids/data/convert_from_color.py
+++ src/test/py/ltprg/game/colorGrids/data/convert_from_color.py
@@ -4,7 +4,7 @@ from mung.data import DataSet, Datum
 
 data_dir = sys.argv[1]
 output_dir = sys.argv[2]
-grid_dimension = int(sys.arg[3])
+grid_dimension = int(sys.argv[3])
 
 D = DataSet.load(data_dir, id_key="gameid")
 grid_data = []
@@ -82,7 +82,7 @@ for datum in D:
                 grid_event["action"]["lClicked"] = int(event["lClickedIndex"])
                 grid_event["action"]["mouseX"] = -1
                 grid_event["action"]["mouseY"] = -1
-                grid_event["action"]["condition"] = { "name" : event["condition"].upper() }
+                grid_event["action"]["condition"] = { "name" : event["condition"].upper(), "gridDimension" : grid_dimension  }
             elif event["type"] == "Utterance":
                 grid_event = event
                 grid_event["eventType"] = "utterance"
