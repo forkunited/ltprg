08bb57c258eb300c5834d74403fc8f5b552eadb5
Bill McDowell <forkunited@gmail.com>
2018-04-25 10:06:33 -0700

    Fixed bugs in refactored data loading

--- src/test/py/ltprg/game/colorGrids/model/learn_RSA.py
+++ src/test/py/ltprg/game/colorGrids/model/learn_RSA.py
@@ -1,4 +1,7 @@
 import argparse
+import torch
+import numpy as np
+import ltprg.data.feature
 
 from mung.util.config import Config
 from mung.feature import MultiviewDataSet, Symbol
@@ -7,13 +10,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('env', action="store")
 parser.add_argument('data', action="store")
 parser.add_argument('--seed', action='store', dest='seed', type=int, default=1)
+parser.add_argument('--gpu', action='store', dest='gpu', type=bool, default=True)
 args = parser.parse_args()
 
-seed = args["seed"]
-env = Config.load(args["env"])
-data_config = Config.load(args["data"], environment=env)
-
-gpu = env["gpu"]
+gpu = args.gpu
+seed = args.seed
+env = Config.load(args.env)
+data_config = Config.load(args.data, environment=env)
 
 if gpu:
     torch.cuda.manual_seed(seed)
@@ -24,4 +27,4 @@ _, S = MultiviewDataSet.load_from_config(data_config)
 
 print "Loaded data..."
 for key in S.keys():
-    print key + ": " + len(S[key])
+    print key + ": " + str(S[key].get_size())
