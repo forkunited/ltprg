cc8476d10cbf6d0fac42808e8520191c624296ad
Bill McDowell <forkunited@gmail.com>
2018-04-25 20:52:30 -0700

    Refactored code for loading rsa and rsa evaluations

--- src/main/py/ltprg/model/rsa.py
+++ src/main/py/ltprg/model/rsa.py
@@ -2,8 +2,14 @@ import torch
 import torch.nn as nn
 import mung.torch_ext.eval
 from torch.autograd import Variable
+from torch.nn import NLLLoss
 from ltprg.model.dist import Categorical
-from mung.torch_ext.eval import DistributionAccuracy
+from ltprg.model.prior import UniformIndexPriorFn, SequenceSamplingPriorFn
+from ltprg.model.meaning import MeaningModel, MeaningModelIndexedWorldSequentialUtterance, SequentialUtteranceInputType
+from ltprg.model.obs import ObservationModel, ObservationModelReorderedSequential
+from ltprg.model.seq import SequenceModel, SequenceModelNoInput, SequenceModelInputToHidden
+from ltprg.model.seq_heuristic import HeuristicL0
+from mung.torch_ext.eval import DistributionAccuracy, Loss
 
 EPSILON=1e-9 # Note, was 1e-6 on color, and not in normalize_rows back then
 
@@ -150,6 +156,127 @@ class RSA(nn.Module):
         else:
             return S(name, level, meaning_fn, world_prior_fn, utterance_prior_fn, L_bottom=L_bottom, soft_bottom=soft_bottom, alpha=alpha, observation_fn=observation_fn)
 
+    # Expects config of the form:
+    # {
+    #   data_parameter : {
+    #     utterance : [UTTERANCE DATA PARAMETER NAME]
+    #     L_world : [L_WORLD DATA PARAMETER NAME]
+    #     L_observation : [L_OBSERVATION DATA PARAMETER NAME]
+    #     S_world : [S_WORLD DATA PARAMETER NAME]
+    #     S_observation : [S_OBSERVATION DATA PARAMETER NAME]
+    #     mode : [S OR L DISTRIBUTION]
+    #     utterance_seq :[INDICATOR OF WHETHER UTTERANCE IS SEQUENTIAL]
+    #   },
+    #   utterance_prior : {
+    #     seq_model_path : [PATH TO STORED UTTERANCE PRIOR SEQUENCE MODEL]
+    #     heuristic : [NAME OF HEURISTIC FOR GUIDING UTTERANCE SAMPLING (L0|None)]
+    #     parameters : {
+    #       training_mode : [SAMPLING MODE DURING TRAINING (FORWARD|BEAM|SMC)] 
+    #       eval_mode : [SAMPLING MODE DURING EVALUATION (FORWARD|BEAM|SMC)]
+    #       samples_per_input : [SAMPLES PER INPUT WITHIN OBSERVATION]
+    #       uniform : [INDICATOR OF WHETHER SAMPLES ARE UNIFORM OR WEIGHTED]
+    #       training_input_mode : [Mode for sampling during training (IGNORE_TRUE_WORLD|ONLY_TRUE_WORLD|None)]                                  
+    #       sample_length : [Length of samples to take]
+    #       n_before_heuristic : [Samples prior to applying heuristic in forward sampling]
+    #     }
+    #   },
+    #   world_prior : {
+    #     support_size : [NUMBER OF WORLDS IN SUPPORT]
+    #   },
+    #   meaning_fn : {
+    #     seq_model : {
+    #       (Optional) model_path : [PATH TO EXISTING MEANING MODEL]
+    #       bidirectional : [INDICATES WHETHER SEQUENCE MDOEL IS BIDIRECTIONAL]
+    #       dropout : [DROPOUT]
+    #       rnn_layers : [RNN_LAYERS]
+    #       rnn_size : [SIZE OF RNN HIDDEN LAYER]
+    #       embedding_size : [EMBEDDING_SIZE]
+    #       rnn_type : [RNN TYPE]
+    #     }
+    #   },
+    #   (Optional) observation_fn : {
+    #     seq_model : {
+    #       (Optional) model_path : [PATH TO EXISTING OBSERVATION MODEL]
+    #       bidirectional : [INDICATES WHETHER SEQUENCE MDOEL IS BIDIRECTIONAL]
+    #       dropout : [DROPOUT]
+    #       rnn_layers : [RNN_LAYERS]
+    #       rnn_size : [SIZE OF RNN HIDDEN LAYER]
+    #       embedding_size : [EMBEDDING_SIZE]
+    #       rnn_type : [RNN TYPE]
+    #       non_emb : [INDICATES WHETHER INPUTS SHOULD BE EMBEDDED PRIOR TO RNN]
+    #     }
+    #   },
+    #   training_level : [RSA LEVEL AT WHICH TO TRAIN]
+    #   alpha : [ALPHA RATIONALITY PARAMETER]
+    # }
+    @staticmethod
+    def load_from_config(config, D, gpu=False):
+        data_parameter = DataParameter.make(**config["data_parameter"])
+        utterance_field = config["data_parameter"]["utterance"]
+        observation_field = config["data_parameter"]["L_observation"]
+        if config["data_parameter"]["mode"] == DistributionType.S:
+            observation_field = config["data_parameter"]["S_observation"]
+        world_support_size = config["world_prior"]["support_size"]
+
+        # Optionally setup observation function if observation is sequential
+        observation_fn = None
+        world_input_size = None
+        if "observation_fn" in config:
+            obs_config = config["observation_fn"]["seq_model"]
+            world_input_size = obs_config["rnn_size"] * obs_config["rnn_layers"]
+            if obs_config["bidirectional"]:
+                world_input_size *= 2
+
+            if "model_path" in obs_config:
+                observation_fn = ObservationModel.load(obs_config["model_path"])
+            else:
+                observation_size = D[observation_field].get_matrix(0).get_feature_set().get_token_count()
+                seq_observation_model = SequenceModelNoInput("Observation", observation_size,
+                    obs_config["embedding_size"], obs_config["rnn_size"], obs_config["rnn_layers"], 
+                    dropout=obs_config["dropout"], rnn_type=obs_config["rnn_type"], bidir=obs_config["bidirectional"], 
+                    non_emb=obs_config["non_emb"])
+                observation_fn = ObservationModelReorderedSequential(world_input_size, world_support_size, seq_observation_model)
+        else:
+            world_input_size = D[observation_field].get_feature_set().get_token_count() / world_support_size
+
+        # Setup meaning function
+        meaning_config = config["meaning_fn"]["seq_model"]
+        meaning_fn = None
+        if "model_path" in meaning_config:
+            meaning_fn = MeaningModel.load(meaning_config["model_path"])
+        else:
+            utterance_size = D[utterance_field].get_matrix(0).get_feature_set().get_token_count()
+            seq_meaning_model = SequenceModelInputToHidden("Meaning", utterance_size, world_input_size, \
+            meaning_config["embedding_size"], meaning_config["rnn_size"], meaning_config["rnn_layers"], \
+            dropout=meaning_config["dropout"], rnn_type=meaning_config["rnn_type"], 
+            bidir=meaning_config["bidirectional"], input_layers=1)
+            meaning_fn = MeaningModelIndexedWorldSequentialUtterance(world_input_size, seq_meaning_model, \
+                input_type=SequentialUtteranceInputType.IN_SEQ)
+
+        # Setup world prior
+        world_prior_fn = UniformIndexPriorFn(world_support_size, on_gpu=gpu, unnorm=False)
+
+        # Setup utterance prior
+        heuristic = None
+        if config["utterance_prior"]["heuristic"] == "L0":
+            heuristic = HeuristicL0(world_prior_fn, meaning_fn, soft_bottom=False)
+
+        seq_prior_model = SequenceModel.load(config["seq_model_path"])
+
+        utterance_prior_params = config["utterance_prior"]
+        utterance_prior_params["seq_length"] = D[utterance_field].get_feature_seq_set.get_size()
+        utterance_prior_params["heuristic"] = heuristic
+        utterance_prior_fn = SequenceSamplingPriorFn(seq_prior_model, world_input_size, **utterance_prior_params)
+        
+        rsa_model = RSA.make(data_parameter.get_mode() + "_" + str(config["training_level"]), \
+                    data_parameter.get_mode(), config["training_level"], meaning_fn, world_prior_fn, 
+                    utterance_prior_fn, L_bottom=True, soft_bottom=False, 
+                    alpha=config["alpha"], observation_fn=observation_fn)
+        if gpu:
+            rsa_model = rsa_model.cuda()
+
+        return rsa_model
+
 class S(RSA):
     def __init__(self, name, level, meaning_fn, world_prior_fn, utterance_prior_fn, L_bottom=True, soft_bottom=False, alpha=1.0, observation_fn=None):
         """
@@ -244,7 +371,7 @@ class S(RSA):
         world_support = None
         ps = None
         if self._level == 0:
-            meaning = self._meaning_fn(utterance, world, observation).transpose(2,1)
+            meaning = self._meaning_fn(utterance_prior.support(), world, observation).transpose(2,1)
             ps = _normalize_rows(utterance_prior.p().unsqueeze(1).expand_as(meaning) * meaning, softmax=self._soft_bottom)
         else:
             l = self._L(utterance_prior.support(), observation, utterance_dist=True)
@@ -502,3 +629,50 @@ class RSADistributionAccuracy(DistributionAccuracy):
         self._distribution_type = distribution_type
         self._L_bottom = L_bottom
         self._data_parameters = data_parameters.to_mode(self._distribution_type)
+
+# Expects config of the form:
+# {
+#    data_parameter : {
+#     utterance : [UTTERANCE DATA PARAMETER NAME]
+#     L_world : [L_WORLD DATA PARAMETER NAME]
+#     L_observation : [L_OBSERVATION DATA PARAMETER NAME]
+#     S_world : [S_WORLD DATA PARAMETER NAME]
+#     S_observation : [S_OBSERVATION DATA PARAMETER NAME]
+#     mode : [S OR L DISTRIBUTION]
+#     utterance_seq :[INDICATOR OF WHETHER UTTERANCE IS SEQUENTIAL]
+#   },
+#   evaluations : [
+#    name : [NAME FOR EVALUATION]
+#    type : (NLLLoss, RSADistributionAccuracy)
+#    data : [NAME OF DATA SUBSET]
+#    (Optional) data_size : [SIZE OF RANDOM SUBET OF DATA TO TAKE]
+#    parameters : {
+#      (RSADistributionAccuracy) dist_level : [RSA DISTRIBUTION LEVEL TO EVALUATE]
+#      (RSADistributionAccuracy) dist_type : [TYPE OF RSA DISTRIBUTION (L|S)]
+#    }
+#   ]
+# }
+def load_evaluations_from_config(config, D, gpu=False):
+    data_parameter = DataParameter.make(**config["data_parameter"])
+    evaluations = []
+
+    loss_criterion = NLLLoss(size_average=False)
+    if gpu:
+        loss_criterion = loss_criterion.cuda()
+
+    for eval_config in config["evaluations"]:
+        data = D[eval_config["data"]]
+        if "data_size" in config:
+            data = data.get_random_subset(eval_config["data_size"])
+
+        if eval_config["type"] == "NLLLoss":
+            loss = Loss(eval_config["name"], data, data_parameter, loss_criterion)
+            evaluations.append(loss)
+        elif eval_config["type"] == "RSADistributionAccuracy":
+            acc = RSADistributionAccuracy(eval_config["name"], eval_config["parameters"]["dist_level"], \
+                eval_config["parameters"]["dist_type"], data, data_parameter)
+            evaluations.append(acc)
+        else:
+            raise ValueError("Invalid RSA evaluation type in config (" + str(eval_config["type"]))
+    return evaluations
+
--- src/test/py/ltprg/game/colorGrids/model/learn_RSA.py
+++ src/test/py/ltprg/game/colorGrids/model/learn_RSA.py
@@ -6,25 +6,39 @@ import ltprg.data.feature
 from mung.util.config import Config
 from mung.feature import MultiviewDataSet, Symbol
 
+from ltprg.model.rsa import RSA, load_evaluations_from_config
+
 parser = argparse.ArgumentParser()
 parser.add_argument('env', action="store")
 parser.add_argument('data', action="store")
+parser.add_argument('model', action="store")
+parser.add_argument('train_evals', action="store")
+parser.add_argument('dev_evals', action="store")
+parser.add_argument('test_evals', action="store")
 parser.add_argument('--seed', action='store', dest='seed', type=int, default=1)
 parser.add_argument('--gpu', action='store', dest='gpu', type=bool, default=True)
+parser.add_argument('--eval_test', action='store', dest='eval_test', type=bool, default=False)
 args = parser.parse_args()
 
 gpu = args.gpu
 seed = args.seed
 env = Config.load(args.env)
 data_config = Config.load(args.data, environment=env)
+model_config = Config.load(args.model, environment=env)
+train_evals_config = Config.load(args.train_evals, environment=env)
+dev_evals_config = Config.load(args.dev_evals, environment=env)
+test_evals_config = Config.load(args.test_evals, environment=env)
 
 if gpu:
     torch.cuda.manual_seed(seed)
 torch.manual_seed(seed)
 np.random.seed(seed)
 
-_, S = MultiviewDataSet.load_from_config(data_config)
+_, data_sets = MultiviewDataSet.load_from_config(data_config)
+rsa = RSA.load_from_config(model_config, data_sets["train"], gpu=gpu)
 
-print "Loaded data..."
-for key in S.keys():
-    print key + ": " + str(S[key].get_size())
+train_evals = load_evaluations_from_config(train_evals_config, data_sets, gpu=False)
+dev_evals = load_evaluations_from_config(dev_evals_config, data_sets, gpu=False)
+test_evals = load_evaluations_from_config(test_evals_config, data_sets, gpu=False)
+
+
