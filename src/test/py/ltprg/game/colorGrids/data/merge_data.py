e7ab01285a8005a1695e35c4204115ba5046024e
Bill McDowell <forkunited@gmail.com>
2018-05-17 13:04:29 -0700

    Modified data merge script for new data

--- src/test/py/ltprg/game/colorGrids/data/merge_data.py
+++ src/test/py/ltprg/game/colorGrids/data/merge_data.py
@@ -2,14 +2,17 @@ import sys
 import numpy as np
 from mung.data import DataSet, Datum, Partition
 
-data_dir_1 = sys.argv[1]
-data_dir_2 = sys.argv[2]
-part_file_1 = sys.argv[3]
-part_file_2 = sys.argv[4]
-data_name_1 = sys.argv[5]
-data_name_2 = sys.argv[6]
-output_dir = sys.argv[7]
-output_part_file = sys.argv[8]
+color_data_dir = sys.argv[1]
+grid_data_dir_1 = sys.argv[2]
+grid_data_dir_2 = sys.argv[3]
+color_part_file = sys.argv[4]
+grid_part_file_1 = sys.argv[5]
+grid_part_file_2 = sys.argv[6]
+color_data_name = sys.argv[7]
+grid_data_name_1 = sys.argv[8]
+grid_data_name_2 = sys.argv[9]
+output_dir = sys.argv[10]
+output_part_file = sys.argv[11]
 
 def add_datum(datums_list, d, d_source_name):
     d_props = d.to_dict()
@@ -18,20 +21,31 @@ def add_datum(datums_list, d, d_source_name):
             for key in event:
                 if isinstance(event[key], dict) and "condition" in event[key]:
                     event[key]["condition"]["source"] = d_source_name
+                    if "diffs" not in event[key]["condition"]:
+                        event[key]["condition"]["numDiffs"] = 9
     datums_list.append(Datum(properties=d_props, id_key="gameid"))
 
-D_1 = DataSet.load(data_dir_1, id_key="gameid")
-D_2 = DataSet.load(data_dir_2, id_key="gameid")
-P_1 = Partition.load(part_file_1)
-P_2 = Partition.load(part_file_2)
+D_color = DataSet.load(color_data_dir, id_key="gameid")
+D_grid_1 = DataSet.load(grid_data_dir_1, id_key="gameid")
+D_grid_2 = DataSet.load(grid_data_dir_2, id_key="gameid")
+P_color = Partition.load(color_part_file)
+P_grid_1 = Partition.load(grid_part_file_1)
+P_grid_2 = Partition.load(grid_part_file_2)
 
 datums_out = []
-for d in D_1:
-    add_datum(datums_out, d, data_name_1)
-for d in D_2:
-    add_datum(datums_out, d, data_name_2)
+for d in D_color:
+    add_datum(datums_out, d, color_data_name)
+for d in D_grid_1:
+    add_datum(datums_out, d, grid_data_name_1)
+for d in D_grid_2:
+    add_datum(datums_out, d, grid_data_name_2)
 D_out = DataSet(data=datums_out, id_key="gameid")
-P_out = P_1.union(P_2)
+
+P_grid_1.merge_parts(["train", "dev"], "train")
+P_grid_1.split_part("train", ["train", "dev"], [0.8, 0.1])
+
+P_out = P_color.union(P_grid_1).union(P_grid_2)
+
 
 D_out.save(output_dir)
 P_out.save(output_part_file)
