709ce8d29f93ad99b1d76cc97c61c27d72ea7495
Bill McDowell <forkunited@gmail.com>
2018-06-06 11:21:04 -0700

    Updated color grid data merging/partitioning scripts

--- src/test/py/ltprg/data/make_split.py
+++ src/test/py/ltprg/data/make_split.py
@@ -1,15 +1,31 @@
 import sys
 import numpy as np
+import argparse
 from mung.data import DataSet, Partition
 
-np.random.seed(1)
-
-SPLIT_SIZES = [0.8,0.1,0.1]
 PART_NAMES = ["train", "dev", "test"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('data_dir', action="store")
+parser.add_argument('split_output_file', action="store")
+parser.add_argument('train_size', action="store", type=float)
+parser.add_argument('dev_size', action="store", type=float)
+parser.add_argument('test_size', action="store", type=float)
+parser.add_argument('--seed', action='store', dest='seed', type=int, default=1)
+parser.add_argument('--maintain_partition_file', action='store', dest='maintain_partition_file', default=None)
+args = parser.parse_args()
+
+np.random.seed(args.seed)
 
 data_dir = sys.argv[1]
 split_output_file = sys.argv[2]
 
+part_sizes = [args.train_size, args.dev_size, args.test_size]
+
+maintain_part = None
+if args.maintain_partition_file is not None:
+    maintain_part = Partition.load(args.maintain_partition_file)
+
 D_all = DataSet.load(data_dir, id_key="gameid")
-partition = Partition.make(D_all, SPLIT_SIZES, PART_NAMES, lambda d : d.get_id())
+partition = Partition.make(D_all, part_sizes, PART_NAMES, lambda d : d.get_id(), maintain_part=maintain_part)
 partition.save(split_output_file)
--- src/test/py/ltprg/game/colorGrids/data/merge_data.py
+++ src/test/py/ltprg/game/colorGrids/data/merge_data.py
@@ -3,16 +3,10 @@ import numpy as np
 from mung.data import DataSet, Datum, Partition
 
 color_data_dir = sys.argv[1]
-grid_data_dir_1 = sys.argv[2]
-grid_data_dir_2 = sys.argv[3]
-color_part_file = sys.argv[4]
-grid_part_file_1 = sys.argv[5]
-grid_part_file_2 = sys.argv[6]
-color_data_name = sys.argv[7]
-grid_data_name_1 = sys.argv[8]
-grid_data_name_2 = sys.argv[9]
-output_dir = sys.argv[10]
-output_part_file = sys.argv[11]
+grid_data_dir = sys.argv[2]
+color_data_name = sys.argv[3]
+grid_data_name = sys.argv[4]
+output_dir = sys.argv[5]
 
 def add_datum(datums_list, d, d_source_name):
     d_props = d.to_dict()
@@ -26,26 +20,13 @@ def add_datum(datums_list, d, d_source_name):
     datums_list.append(Datum(properties=d_props, id_key="gameid"))
 
 D_color = DataSet.load(color_data_dir, id_key="gameid")
-D_grid_1 = DataSet.load(grid_data_dir_1, id_key="gameid")
-D_grid_2 = DataSet.load(grid_data_dir_2, id_key="gameid")
-P_color = Partition.load(color_part_file)
-P_grid_1 = Partition.load(grid_part_file_1)
-P_grid_2 = Partition.load(grid_part_file_2)
+D_grid = DataSet.load(grid_data_dir, id_key="gameid")
 
 datums_out = []
 for d in D_color:
     add_datum(datums_out, d, color_data_name)
-for d in D_grid_1:
-    add_datum(datums_out, d, grid_data_name_1)
-for d in D_grid_2:
-    add_datum(datums_out, d, grid_data_name_2)
+for d in D_grid:
+    add_datum(datums_out, d, grid_data_name)
 D_out = DataSet(data=datums_out, id_key="gameid")
 
-P_grid_1.merge_parts(["train", "dev"], "train")
-P_grid_1.split_part("train", ["train", "dev"], [0.8, 0.1])
-
-P_out = P_color.union(P_grid_1).union(P_grid_2)
-
-
 D_out.save(output_dir)
-P_out.save(output_part_file)
